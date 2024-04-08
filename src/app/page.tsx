0d6b95537f3d7080ff4bd71664b9c0a37f8c2b55
arctouch-danielbastos <daniel.bastos@quizlet.com>
2024-04-08 14:50:01 -0300

    Theming

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Button from "ui/Button";
+import Book from "@material-design-icons/svg/outlined/book.svg";
 
 export default function Memorizer() {
   return (
     <main style={{ display: "flex", gap: "15px" }}>
-      <Button type="text">Cancelar</Button>
+      <Button type="tonal">Cancelar</Button>
       <Button>Decorar</Button>
-      <Button type="tonal">Voltar</Button>
+      <Button icon={<Book />} disabled>
+        Decorar
+      </Button>
     </main>
   );
 }
--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -1,9 +1,14 @@
 import { ReactNode } from "react";
-import style from "ui/Button.module.css";
+import style from "ui/Button.module.scss";
 
 type Variant = "filled" | "tonal" | "text";
 
-type Props = { children: string; type?: Variant; icon?: ReactNode };
+type Props = {
+  children: string;
+  disabled?: boolean;
+  type?: Variant;
+  icon?: ReactNode;
+};
 
 const variantCx: { [variant in Variant]: keyof typeof style } = {
   filled: style.filled,
@@ -11,14 +16,20 @@ const variantCx: { [variant in Variant]: keyof typeof style } = {
   tonal: style.tonal,
 };
 
-export default function Button({ children, icon, type = "filled" }: Props) {
+export default function Button({
+  children,
+  disabled = false,
+  icon,
+  type = "filled",
+}: Props) {
   const cx = `${style.button} ${variantCx[type]}`;
   const shouldRenderIcon = icon || type !== "text";
 
   return (
-    <button className={cx}>
+    <button className={cx} disabled={disabled}>
+      <div className={style.state} />
       {shouldRenderIcon && <span className={style.icon}>{icon}</span>}
-      {children}
+      <div>{children}</div>
     </button>
   );
 }
