2b7ccf06e86f483a72969b413a0b0a7530c5fa9f
arctouch-danielbastos <daniel.bastos@quizlet.com>
2024-04-09 17:19:31 -0300

    Fix colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,10 @@ import TopBar from "ui/TopBar";
 export default function Memorizer() {
   return (
     <main style={{ display: "grid", width: "100%" }}>
-      <TopBar title="Hebreus 4:14-16" right={<IconButton icon="book" />} />
+      <TopBar
+        title="Hebreus 4:14-16"
+        right={<IconButton type="tonal" icon="book" />}
+      />
     </main>
   );
 }
