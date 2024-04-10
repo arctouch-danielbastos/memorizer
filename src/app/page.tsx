c4ef3de4ce25e2c4f41ace45f501ca28f9d1f433
arctouch-danielbastos <daniel.bastos@quizlet.com>
2024-04-09 22:04:21 -0300

    FAB

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
+import FAB from "ui/FAB";
 import IconButton from "ui/IconButton";
 import TopBar from "ui/TopBar";
+
+const Row = ({ children }) => (
+  <div
+    style={{
+      display: "flex",
+      gap: "10px",
+      alignItems: "center",
+      margin: "30px",
+    }}
+  >
+    {children}
+  </div>
+);
 
 export default function Memorizer() {
   return (
@@ -8,6 +22,46 @@ export default function Memorizer() {
         title="Hebreus 4:14-16"
         right={<IconButton type="tonal" icon="book" />}
       />
+      <Row>
+        <FAB icon="home" size="small" />
+        <FAB icon="home" />
+        <FAB icon="home" size="large" />
+        <FAB icon="home" lowered size="small" />
+        <FAB icon="home" lowered />
+        <FAB icon="home" lowered size="large" />
+      </Row>
+      <Row>
+        <FAB icon="home" type="secondary" size="small" />
+        <FAB icon="home" type="secondary" />
+        <FAB icon="home" type="secondary" size="large" />
+        <FAB icon="home" type="secondary" lowered size="small" />
+        <FAB icon="home" type="secondary" lowered />
+        <FAB icon="home" type="secondary" lowered size="large" />
+      </Row>
+      <Row>
+        <FAB icon="home" type="tertiary" size="small" />
+        <FAB icon="home" type="tertiary" />
+        <FAB icon="home" type="tertiary" size="large" />
+        <FAB icon="home" type="tertiary" lowered size="small" />
+        <FAB icon="home" type="tertiary" lowered />
+        <FAB icon="home" type="tertiary" lowered size="large" />
+      </Row>
+      <Row>
+        <FAB icon="home" type="surface" size="small" />
+        <FAB icon="home" type="surface" />
+        <FAB icon="home" type="surface" size="large" />
+        <FAB icon="home" type="surface" lowered size="small" />
+        <FAB icon="home" type="surface" lowered />
+        <FAB icon="home" type="surface" lowered size="large" />
+      </Row>
+      <Row>
+        <FAB icon="home" type="branded" size="small" />
+        <FAB icon="home" type="branded" />
+        <FAB icon="home" type="branded" size="large" />
+        <FAB icon="home" type="branded" lowered size="small" />
+        <FAB icon="home" type="branded" lowered />
+        <FAB icon="home" type="branded" lowered size="large" />
+      </Row>
     </main>
   );
 }
---
+++ src/ui/FAB.tsx
@@ -0,0 +1,44 @@
+import style from "ui/Fab.module.scss";
+import { ICON_CLASS } from "./contants";
+import { MaterialIcon } from "@material-design-icons/font";
+
+type Variant = "primary" | "secondary" | "tertiary" | "branded" | "surface";
+type Size = "small" | "default" | "large";
+
+type Props = {
+  type?: Variant;
+  icon: MaterialIcon;
+  size?: Size;
+  lowered?: boolean;
+};
+
+const variantCx: { [variant in Variant]: keyof typeof style } = {
+  primary: style.primary,
+  secondary: style.secondary,
+  tertiary: style.tertiary,
+  branded: style.branded,
+  surface: style.surface,
+};
+
+const sizeCx: { [variant in Size]: keyof typeof style } = {
+  small: style.small,
+  default: style.default,
+  large: style.large,
+};
+
+export default function FAB({
+  icon,
+  lowered = false,
+  type = "primary",
+  size = "default",
+}: Props) {
+  let cx = `${style.root} ${variantCx[type]} ${sizeCx[size]}`;
+  if (lowered) cx += ` ${style.lowered}`;
+
+  return (
+    <button className={cx}>
+      <div className={style.state} />
+      <div className={ICON_CLASS}>{icon}</div>
+    </button>
+  );
+}
