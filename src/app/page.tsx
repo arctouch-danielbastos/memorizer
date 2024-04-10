135e5abf63a9d1e0022b9735c357ed60887fe674
arctouch-danielbastos <daniel.bastos@quizlet.com>
2024-04-10 09:49:29 -0300

    Style fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,14 @@ export default function Memorizer() {
       />
       <div className={style.body}>
         <article>
-          Portanto, visto que temos um grande sumo sacerdote que adentrou os céus, Jesus, o Filho de Deus, apeguemo‑nos com toda a firmeza ao que confessamos, pois não temos um sumo sacerdote que não possa compadecer‑se das nossas fraquezas, mas sim alguém que, como nós,passou por todo tipo de tentação, ainda que sem pecado. A___, a______‑n__ d_ t____ da g____ c__ t___ a c_________, a f__ de r_________ m___________ e e__________ g____ q__ n__ a____ no m_______ da n__________. 
+          Portanto, visto que temos um grande sumo sacerdote que adentrou os
+          céus, Jesus, o Filho de Deus, apeguemo‑nos com toda a firmeza ao que
+          confessamos, pois não temos um sumo sacerdote que não possa
+          compadecer‑se das nossas fraquezas, mas sim alguém que, como
+          nós,passou por todo tipo de tentação, ainda que sem pecado. A___,
+          a______‑n__ d_ t____ da g____ c__ t___ a c_________, a f__ de
+          r_________ m___________ e e__________ g____ q__ n__ a____ no m_______
+          da n__________.
         </article>
         <div className={style.progress}>
           <ProgressIndicator progress={30} />
